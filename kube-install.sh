b417f043e7b2ccf32348de8523d26df4de8f5aea
Trần Phan An Trường <66484965+beta21s@users.noreply.github.com>
2023-06-21 08:57:03 +0700

    Update kube-install.sh

--- kube-install.sh
+++ kube-install.sh
@@ -65,9 +65,7 @@ function k8s-install {
 
  	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add 
 
-	cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+	cat <<EOF >/etc/apt/sources.list.d/kubernetes.list \\ deb http://apt.kubernetes.io/ kubernetes-xenial main \\ EOF
  
  	sudo apt-get update
 	echo -e "${GREEN} installing kubectl kubeadm kubelet...${NC}"
